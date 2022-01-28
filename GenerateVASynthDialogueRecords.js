8fce0ddc5c65bac31f4820df30e9d4e59601c928
megaman2k <megaman2kx@gmail.com>
2022-01-27 19:47:18 -0800

    Adds basic info/response creation.

--- GenerateVASynthDialogueRecords.js
+++ GenerateVASynthDialogueRecords.js
@@ -50,17 +50,81 @@ const topics = {
 Object.keys(topics).forEach(key => 
   topics[key].editorId = voice.fullName + '_Topic_' + topics[key].name);
 
+const commonConditions = {
+  isNewVoice: {
+    comparisonValue: '1.0',
+    function: 'GetIsVoiceType',
+    parameter1: voice.fullName,
+    type: '10000000'
+  }
+};
+
+// Infos can be shared or not. Shared ones have editorIds.
+// Infos can use a shared response data or not. Those that do reference the above editorIds. The others have "Response" objects.
+const infos = [
+  // shared
+  {topic: 'Shared', editorId: voice.fullName + '_Shared_Yes', conditions: [commonConditions.isNewVoice], responses: [
+    {text: 'Yes.', emotionType: 'Neutral', emotionValue: 50}
+  ]},
+  // non-shared, uses shared response
+  {topic: 'Agree', flags: ['Random'], conditions: [commonConditions.isNewVoice], responseData: voice.fullName + '_Shared_Yes'},
+  // non-shared, uses own response
+  {topic: 'Agree', flags: ['Random', 'Goodbye'], conditions: [commonConditions.isNewVoice], responses: [
+     {text: 'Alright. Fine.', emotionType: 'Neutral', emotionValue: 50}
+  ]}
+];
+
 let plugin = xelib.FileByName(pluginName);
 xelib.WithHandle(plugin, function() {
-  createVoiceType(plugin, voice);
-  createQuest(plugin, quest);
-  Object.keys(topics).forEach(key => createTopic(plugin, quest, topics[key]));
-  Object.keys(branches).forEach(key => createBranch(plugin, quest, branches[key], topics));
-  //addResponses(plugin, voice);
+  // createVoiceType(plugin, voice);
+  // createQuest(plugin, quest);
+  // Object.keys(topics).forEach(key => createTopic(plugin, quest, topics[key]));
+  // Object.keys(branches).forEach(key => createBranch(plugin, quest, branches[key], topics));
+  infos.forEach(info => createInfo(plugin, topics, info));
 });
 
-function addResponses(plugin, voice, topic) {
-  zedit.log('Adding dialogue responses...');
+function createInfo(plugin, topics, info) {
+  let topic = topics[info.topic];
+  let element = xelib.AddElement(plugin, 'DIAL\\' + topic.editorId + '\\INFO\\');
+
+  if ('editorId' in info) maybeAddElementValue(element, 'EDID', info.editorId);
+  if ('conditions' in info) info.conditions.forEach(condition => createCondition(element, condition));
+  let i = 0;
+  if ('responses' in info) info.responses.forEach(response => createResponse(element, response, i++));
+  if ('flags' in info) {
+    let f = xelib.AddElement(element, 'ENAM');
+    xelib.SetEnabledFlags(f, 'Flags', info.flags);
+  }
+  if ('responseData' in info) xelib.AddElementValue(element, 'DNAM', info.responseData);
+
+  // Mandatory elements/values:
+  xelib.AddElement(element, 'CNAM'); // Favor Level: defaults to "None"
+  xelib.AddElement(element, 'PNAM'); // Previous INFO: defaults to "NULL"
+  // We need to find the previous response in this topic and set PNAM to it.
+  let infoGroup = xelib.GetElement(plugin, 'DIAL\\' + topic.editorId + '\\Child Group');
+  xelib.WithHandle(infoGroup, function() {
+    let count = xelib.ElementCount(infoGroup);
+    let previousInfoIndex = count - 2;
+    if (previousInfoIndex >= 0) {
+      let previousInfo = xelib.GetElements(infoGroup)[previousInfoIndex];
+      xelib.SetValue(element, 'PNAM', xelib.GetHexFormID(previousInfo));
+    }
+  });
+}
+
+function createCondition(infoElement, condition) {
+  let element = xelib.AddArrayItem(infoElement, 'Conditions', 'CTDA');
+  xelib.SetValue(element, 'CTDA\\Function', condition.function);
+  xelib.SetValue(element, 'CTDA\\Parameter #1', condition.parameter1);
+  xelib.SetValue(element, 'CTDA\\Type', condition.type);
+  xelib.SetValue(element, 'CTDA\\Comparison Value', condition.comparisonValue);
+}
+
+function createResponse(infoElement, response, i) {
+  let element = xelib.AddArrayItem(infoElement, 'Responses');
+  xelib.SetValue(element, 'TRDT\\Emotion Type', response.emotionType);
+  xelib.SetValue(element, 'TRDT\\Emotion Value', response.emotionValue.toString());
+  xelib.AddElementValue(infoElement, 'Responses\\[' + i.toString() + ']\\NAM1', response.text);
 }
 
 function createVoiceType(plugin, voice) {
@@ -163,6 +227,20 @@ function createBranch(plugin, quest, branch, topics) {
  *                              HELPER FUNCTIONS 
  * ----------------------------------------------------------------------------
  */
+
+function logElement(element) {
+  zedit.log('============================');
+  zedit.log('Name: ' + xelib.Name(element));
+  zedit.log('LongName: ' + xelib.LongName(element));
+  zedit.log('DisplayName: ' + xelib.DisplayName(element));
+  zedit.log('Path: ' + xelib.Path(element));
+  zedit.log('LongPath: ' + xelib.LongPath(element));
+  zedit.log('LocalPath: ' + xelib.LocalPath(element));
+  zedit.log('Signature: ' + xelib.Signature(element));
+  zedit.log('Child Elements:');
+  logChildElements(element);
+  zedit.log('============================');
+}
 
 function logChildElements(rootElement) {
   xelib.GetElements(rootElement, '', false).forEach(element => {
